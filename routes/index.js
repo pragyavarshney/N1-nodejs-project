adaab012541d230fb1d61b74a62ba4ab55522327
pragyavarshney <varshney.pragya@gmail.com>
2019-09-30 19:04:45 -0700

    check6

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,34 @@
-//**************************************************************************
-//***** mongodb get all of the Routes in Routes collection where frequence>=1
-//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
-app.get('/mongodb', function (request, response) {
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
-  mongodb.MongoClient.connect("mongodb://<dbuser>:<dbpassword>@ds211275.mlab.com:11275/heroku_mgqgm6t8", function(err, db) {
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hello how are u ' });
+});
+
+//**************************
+//*** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+router.get('/mongodb', function (request, response, next) {
+  MongoClient.connect("mongodb://varshney:varshney1@ds211275.mlab.com:11275/heroku_mgqgm6t8", function(err, database) {
     if(err) throw err;
     //get collection of routes
-    var Routes = db.collection('Routes');
+    const myAwesomeDB = database.db('heroku_mgqgm6t8')
+    var Routes = myAwesomeDB.collection('Routes');
     //get all Routes with frequency >=1
     Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
       if(err) throw err;
 
-      response.render('pages/mongodb', {results: docs});
+      response.render('mongodb', {results: docs});
 
     });
 
     //close connection when your app is terminating.
-    db.close(function (err) {
+    database.close(function (err) {
       if(err) throw err;
     });
   });//end of connect
 });//end app.get
+
+module.exports = router;
